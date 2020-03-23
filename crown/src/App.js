2909b27014b8107bb3f20e33eef534836f38a53c
joblul <jhoque@maristak.net>
2020-03-23 23:16:48 +0100

    created project with create react app and added files for homepage.

---
+++ crown/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Homepage from './homepage.component';
+import './homepage.styles.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage></Homepage>
+    </div>
+  );
+}
+
+export default App;
